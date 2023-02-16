c197af88f8ede6685e70d0b41ac703f8ccd57124
KavinduNimesh1030 <kavindunimesh286@gmail.com>
2023-02-16 11:27:10 +0530

    spa created

--- BackEnd/src/main/java/lk/ijse/spring/entity/Customer.java
+++ BackEnd/src/main/java/lk/ijse/spring/entity/Customer.java
@@ -1,4 +1,26 @@
 package lk.ijse.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+@Entity
 public class Customer {
+    @Id
+    String id;
+    String email;
+    String password;
+    String address;
+    String contactNo;
+    String nic;
+    String dln;
 }
