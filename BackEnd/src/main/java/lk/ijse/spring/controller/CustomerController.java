72bd71b3e91d6ccf7421352551a1325e7bfb108c
KavinduNimesh1030 <kavindunimesh286@gmail.com>
2023-02-16 13:14:39 +0530

    customer controller updated

--- BackEnd/src/main/java/lk/ijse/spring/controller/CustomerController.java
+++ BackEnd/src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -1,7 +1,14 @@
 package lk.ijse.spring.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import lk.ijse.spring.dto.CustomerDTO;
+import lk.ijse.spring.util.ResponseUtil;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/customer")
+@CrossOrigin
 public class CustomerController {
+    public ResponseUtil saveCus(@RequestBody CustomerDTO customerDTO){
+        return new ResponseUtil();
+    }
 }
---
+++ FrontEnd/controller/CustomerRegistration.js
@@ -0,0 +1,23 @@
+$("#submitCusRegDetail").click(function (){
+    var formData = $("#cusRegForm").serialize();
+    console.log(formData);
+
+    $.ajax({
+        url: baseUrl+"Customer",
+        method :"post",
+        data : formData,
+        success: function (resp) {
+            console.log(resp);
+            alert(resp.message);
+
+        },
+        error: function(error) {
+            let prase = JSON.parse(error.responseText);
+            alert(prase.message);
+
+        }
+    });
+
+
+
+});
