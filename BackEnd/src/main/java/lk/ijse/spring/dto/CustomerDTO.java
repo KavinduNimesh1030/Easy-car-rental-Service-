1c27d20fefc42b81117214310c0469e4c3620f75
KavinduNimesh1030 <kavindunimesh286@gmail.com>
2023-02-16 10:38:40 +0530

    customer dto refactored

--- BackEnd/src/main/java/lk/ijse/spring/dto/CustomerDTO.java
+++ BackEnd/src/main/java/lk/ijse/spring/dto/CustomerDTO.java
@@ -1,4 +1,20 @@
 package lk.ijse.spring.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
 public class CustomerDTO {
+    String id;
+    String email;
+    String password;
+    String address;
+    String contactNo;
+    String nic;
+    String dln;
 }
