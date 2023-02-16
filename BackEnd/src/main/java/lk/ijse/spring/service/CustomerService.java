d1277846bfc44b64b6ae8716546f4ddbc0dc50c6
KavinduNimesh1030 <kavindunimesh286@gmail.com>
2023-02-16 13:28:18 +0530

    updated

--- BackEnd/src/main/java/lk/ijse/spring/service/CustomerService.java
+++ BackEnd/src/main/java/lk/ijse/spring/service/CustomerService.java
@@ -1,7 +1,10 @@
 package lk.ijse.spring.service;
 
+import lk.ijse.spring.dto.CustomerDTO;
 import lk.ijse.spring.entity.Customer;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface CustomerService {
-    void saveCustomer();
+    void saveCustomer(CustomerDTO customerDTO);
 }
--- BackEnd/src/main/java/lk/ijse/spring/service/impl/CustomerServiceImpl.java
+++ BackEnd/src/main/java/lk/ijse/spring/service/impl/CustomerServiceImpl.java
@@ -1,4 +1,27 @@
 package lk.ijse.spring.service.impl;
 
-public class CustomerServiceImpl {
+import lk.ijse.spring.dto.CustomerDTO;
+import lk.ijse.spring.entity.Customer;
+import lk.ijse.spring.repo.CustomerRepo;
+import lk.ijse.spring.service.CustomerService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class CustomerServiceImpl implements CustomerService {
+
+    @Autowired
+    CustomerRepo repo;
+
+    @Autowired
+    ModelMapper modelMapper;
+
+    @Override
+    public void saveCustomer(CustomerDTO customerDTO) {
+        Customer entity = modelMapper.map(customerDTO, Customer.class);
+        repo.save(entity);
+    }
 }
