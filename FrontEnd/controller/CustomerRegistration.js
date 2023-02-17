b8d34f23895c88e2f4c710447195d0fa1adc73bd
KavinduNimesh1030 <kavindunimesh286@gmail.com>
2023-02-18 00:03:06 +0530

    refactored customer registation

--- FrontEnd/controller/CustomerRegistration.js
+++ FrontEnd/controller/CustomerRegistration.js
@@ -1,16 +1,35 @@
 let baseUrl = "http://localhost:8080/BackEnd_war/";
 $("#submitCusRegDetail").click(function (){
     var formData = $("#cusRegForm").serialize();
-    console.log(formData);
+   /* var formData = new FormData();*/
+
+
+/*    let id = $("#txtCusRegId").val();
+    let email = $("#txtCusEmail").val();
+    let password = $("#txtPassword").val();
+    let address = $("#txtCusRegId").val();
+    let contactNo = $("#txtContactNo").val();
+    let nic = $("#txtNicNo").val();
+    let dln = $("#txtDrivingLNo").val();
 
     let nicImgPath = $("#formFileNic")[0].files[0].name;
-    let dlnImgPath = $("#formFileDL")[0].files[0];
+    let dlnImgPath = $("#formFileDL")[0].files[0].name;
+    let file = $("#formFileDL")[0].files[0];
+    let file1 =$("#formFileNic")[0].files[0];
+    console.log(nicImgPath);*/
 
-    console.log(nicImgPath);
-
-
+    /*formData.append("id",id);
+    formData.append("email",email);
+    formData.append("password",password);
+    formData.append("address",address);
+    formData.append("contactNo",contactNo);
+    formData.append("nic",nic);
+    formData.append("dln",dln);
     formData.append("nicImgPath",nicImgPath);
-    formData.append("dlnImgPath",dlnImgPath);
+    formData.append("dlnImgPath",dlnImgPath);*/
+
+
+    console.log(formData);
 
     $.ajax({
         url: baseUrl+"Customer",
