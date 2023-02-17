26568ec5bd7d6ed45036a92daf99d48844193529
KavinduNimesh1030 <kavindunimesh286@gmail.com>
2023-02-18 01:31:49 +0530

    appWideExceptionHandler updated

--- BackEnd/src/main/java/lk/ijse/spring/config/WebAppConfig.java
+++ BackEnd/src/main/java/lk/ijse/spring/config/WebAppConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@ComponentScan(basePackageClasses = {CustomerController.class, AppWideExceptionHandler.class})
+@ComponentScan(basePackageClasses = {CustomerController.class,AppWideExceptionHandler.class})
 @EnableWebMvc
 public class WebAppConfig {
     public WebAppConfig() {
