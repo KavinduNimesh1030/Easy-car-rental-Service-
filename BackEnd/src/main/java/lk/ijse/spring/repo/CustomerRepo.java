f19397eac8cb2649151f5d5b3a7516babd586a16
KavinduNimesh1030 <kavindunimesh286@gmail.com>
2023-02-22 10:02:48 +0530

    created vehicle repo

--- BackEnd/src/main/java/lk/ijse/spring/repo/CustomerRepo.java
+++ BackEnd/src/main/java/lk/ijse/spring/repo/CustomerRepo.java
@@ -2,6 +2,7 @@ package lk.ijse.spring.repo;
 
 import lk.ijse.spring.dto.CustomerDTO;
 import lk.ijse.spring.entity.Customer;
+import lk.ijse.spring.entity.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
@@ -9,5 +10,6 @@ import org.springframework.stereotype.Component;
 public interface CustomerRepo extends JpaRepository<Customer,String> {
 
     Customer findCustomerByEmail(String email);
+
 
 }
