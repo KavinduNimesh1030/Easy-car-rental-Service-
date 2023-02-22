e7ff59cc77c2993544cc8524124e639d12015643
KavinduNimesh1030 <kavindunimesh286@gmail.com>
2023-02-22 09:58:48 +0530

    vehicle controller udated

--- BackEnd/src/main/java/lk/ijse/spring/controller/VehicleController.java
+++ BackEnd/src/main/java/lk/ijse/spring/controller/VehicleController.java
@@ -1,4 +1,17 @@
 package lk.ijse.spring.controller;
 
+import lk.ijse.spring.util.ResponseUtil;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Vehicle")
+@CrossOrigin
 public class VehicleController {
+    @PostMapping(params = "vid")
+    public ResponseUtil getVehicleDetail(String vid){
+        
+    }
 }
