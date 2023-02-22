744d51d135d4b1783112ab9eea15fdd1150c4840
KavinduNimesh1030 <kavindunimesh286@gmail.com>
2023-02-22 21:03:53 +0530

    add to cart table updated

--- FrontEnd/controller/CartDetail.js
+++ FrontEnd/controller/CartDetail.js
@@ -8,7 +8,7 @@ function getVehicleDetail(id){
             console.log(resp);
             console.log(resp.data)
 
-                $("#tblAddToCart").append("<tr><td>"+resp.data.vid+"</td><td>"+resp.data.brand+"</td><td>"+resp.data.dailyPrice+"</td><td>"+resp.data.monthlyPrice+"</td><td>"+resp.data.priceForExtraKm+"</td></tr>")
+                $("#tblAddToCart").append("<tr style='height: 71px;column-gap: 10px;gap: 10px;box-shadow: 0.3em 0.3em 1em rgba(0, 0, 0, 0.3);border-radius: 10px;'><td>"+resp.data.vid+"</td><td>"+resp.data.brand+"</td><td>"+resp.data.dailyPrice+"</td><td>"+resp.data.monthlyPrice+"</td><td>"+resp.data.priceForExtraKm+"</td><td>"+resp.data.freeKmForDay+"</td><td>"+resp.data.freeKmForMonth+"</td></tr>")
         },
         error: function(error) {
             let prase = JSON.parse(error.responseText);
