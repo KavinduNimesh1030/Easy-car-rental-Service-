67ae84628986393d3f5f4ec4ff8eb49b4b2f3248
KavinduNimesh1030 <kavindunimesh286@gmail.com>
2023-02-22 10:12:09 +0530

    vehicle serviceImpl clases updated

--- BackEnd/src/main/java/lk/ijse/spring/service/impl/VehicleServiceImpl.java
+++ BackEnd/src/main/java/lk/ijse/spring/service/impl/VehicleServiceImpl.java
@@ -1,12 +1,25 @@
 package lk.ijse.spring.service.impl;
 
 import lk.ijse.spring.dto.VehicleDTO;
+import lk.ijse.spring.repo.VehicleRepo;
 import lk.ijse.spring.service.VehicleService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
+@Transactional
 public class VehicleServiceImpl implements VehicleService {
+
+    @Autowired
+    ModelMapper modelMapper;
+
+    @Autowired
+    VehicleRepo vehicleRepo;
 
     @Override
     public VehicleDTO getVehicleDetail(String vid) {
-        return null;
+        return modelMapper.map(vehicleRepo.findVehicleByVid(vid),VehicleDTO.class);
     }
 }
