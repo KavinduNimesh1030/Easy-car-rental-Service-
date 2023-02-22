cd533551ec3a0f283dd48ecc5342265134e58c96
KavinduNimesh1030 <kavindunimesh286@gmail.com>
2023-02-22 09:55:16 +0530

    sent request using ajax

--- FrontEnd/controller/CartDetail.js
+++ FrontEnd/controller/CartDetail.js
@@ -1,3 +1,16 @@
 function getVehicleDetail(id){
+    $.ajax({
+        url: baseUrl+"Vehicle?vid="+id,
+        method :"get",
+        dataType:"json",
+        success: function (resp) {
+            console.log(resp);
+        },
+        error: function(error) {
+            let prase = JSON.parse(error.responseText);
+            console.log(prase.message);
 
+        }
+
+    });
 }
