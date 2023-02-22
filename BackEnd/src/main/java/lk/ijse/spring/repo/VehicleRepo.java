ece12aaebf37afc0c508a6c56f541a07636b8ed7
KavinduNimesh1030 <kavindunimesh286@gmail.com>
2023-02-22 10:04:15 +0530

    vehicle repo updated

--- BackEnd/src/main/java/lk/ijse/spring/repo/VehicleRepo.java
+++ BackEnd/src/main/java/lk/ijse/spring/repo/VehicleRepo.java
@@ -4,4 +4,5 @@ import lk.ijse.spring.entity.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface VehicleRepo extends JpaRepository<Vehicle,String> {
+    Vehicle findVehicleByVid(String vid);
 }
