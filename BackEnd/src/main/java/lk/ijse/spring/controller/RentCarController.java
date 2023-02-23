960af419ab1448ef050469b03af52f3ac2349b5b
KavinduNimesh1030 <kavindunimesh286@gmail.com>
2023-02-23 15:27:39 +0530

    rent car controller updated

--- BackEnd/src/main/java/lk/ijse/spring/controller/RentCarController.java
+++ BackEnd/src/main/java/lk/ijse/spring/controller/RentCarController.java
@@ -1,6 +1,7 @@
 package lk.ijse.spring.controller;
 
 import lk.ijse.spring.dto.RentalDTO;
+import lk.ijse.spring.service.RentCarService;
 import lk.ijse.spring.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -10,9 +11,12 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin
 public class RentCarController {
 
+    @Autowired
+    RentCarService rentCarService;
 
     @PostMapping
     public ResponseUtil saveRentDetails(@RequestBody RentalDTO rentalDTO){
-        return new ResponseUtil();
+        rentCarService.saveRentDetail(rentalDTO);
+        return new ResponseUtil("200","Saved!",null);
     }
 }
