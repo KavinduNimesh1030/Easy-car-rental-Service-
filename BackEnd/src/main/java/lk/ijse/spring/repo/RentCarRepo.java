3902bb6b494beac5e843fd0554c2ea254546557e
KavinduNimesh1030 <kavindunimesh286@gmail.com>
2023-02-23 15:22:50 +0530

    rent car repo created

---
+++ BackEnd/src/main/java/lk/ijse/spring/repo/RentCarRepo.java
@@ -0,0 +1,8 @@
+package lk.ijse.spring.repo;
+
+import lk.ijse.spring.entity.Rental;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RentCarRepo extends JpaRepository<Rental,String> {
+    
+}
