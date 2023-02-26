efd7f348d8c68fa2c72a8d9ff81cfff7a75e3637
KavinduNimesh1030 <kavindunimesh286@gmail.com>
2023-02-26 23:49:56 +0530

    customer service and impl updated

--- BackEnd/src/main/java/lk/ijse/spring/service/CustomerService.java
+++ BackEnd/src/main/java/lk/ijse/spring/service/CustomerService.java
@@ -5,9 +5,12 @@ import lk.ijse.spring.dto.VehicleDTO;
 import lk.ijse.spring.entity.Customer;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+
 @Component
 public interface CustomerService {
     void saveCustomer(CustomerDTO customerDTO);
     CustomerDTO CheckCustomer(String email);
+    ArrayList<CustomerDTO>getAllCustomers();
 
 }
--- BackEnd/src/main/java/lk/ijse/spring/service/impl/CustomerServiceImpl.java
+++ BackEnd/src/main/java/lk/ijse/spring/service/impl/CustomerServiceImpl.java
@@ -1,14 +1,18 @@
 package lk.ijse.spring.service.impl;
 
 import lk.ijse.spring.dto.CustomerDTO;
+import lk.ijse.spring.dto.RentalDTO;
 import lk.ijse.spring.dto.VehicleDTO;
 import lk.ijse.spring.entity.Customer;
 import lk.ijse.spring.repo.CustomerRepo;
 import lk.ijse.spring.service.CustomerService;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
 
 @Service
 @Transactional
@@ -29,6 +33,11 @@ public class CustomerServiceImpl implements CustomerService {
     @Override
     public CustomerDTO CheckCustomer(String email) {
         return modelMapper.map(repo.findCustomerByEmail(email),CustomerDTO.class);
+    }
+
+    @Override
+    public ArrayList<CustomerDTO> getAllCustomers() {
+        return  modelMapper.map(repo.findAll(),new TypeToken<ArrayList<CustomerDTO>>(){}.getType());
     }
 
 
