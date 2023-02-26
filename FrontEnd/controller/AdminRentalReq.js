2d6b6c1e4de3d9185257e41c0772ffa55b687288
KavinduNimesh1030 <kavindunimesh286@gmail.com>
2023-02-26 12:43:40 +0530

    set ajax for get all rental detail

---
+++ FrontEnd/controller/AdminRentalReq.js
@@ -0,0 +1,21 @@
+getAllRentalDetails();
+function getAllRentalDetails(){
+    console.log(id);
+    $.ajax({
+        url: baseUrl+"Rental",
+        method :"get",
+        dataType:"json",
+        success: function (resp) {
+            console.log(resp);
+            console.log(resp.data)
+
+/*      $("#tblAddToCart").append("<tr style='height: 71px;column-gap: 10px;gap: 10px;box-shadow: 0.3em 0.3em 1em rgba(0, 0, 0, 0.3);border-radius: 10px;'><td>"+resp.data.vid+"</td><td>"+resp.data.brand+"</td><td>"+resp.data.dailyPrice+"</td><td>"+resp.data.monthlyPrice+"</td><td>"+resp.data.priceForExtraKm+"</td><td>"+resp.data.freeKmForDay+"</td><td>"+resp.data.freeKmForMonth+"</td></tr>")*/
+        },
+        error: function(error) {
+            let prase = JSON.parse(error.responseText);
+            console.log(prase.message);
+
+        }
+
+    });
+}
