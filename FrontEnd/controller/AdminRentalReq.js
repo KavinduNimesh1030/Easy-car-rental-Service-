62d405f2b42a7b73d7decad306f45321d09e2633
KavinduNimesh1030 <kavindunimesh286@gmail.com>
2023-02-26 16:49:59 +0530

    Admin rental request updated

--- FrontEnd/controller/AdminRentalReq.js
+++ FrontEnd/controller/AdminRentalReq.js
@@ -7,9 +7,8 @@ function getAllRentalDetails(){
         dataType:"json",
         success: function (resp) {
             for (const r of resp.data) {
-                alert(r.rentId);
                 console.log(r.rentId);
-                $("#tblCustomerReq").append("<tr style='height: 71px;column-gap: 10px;gap: 10px;box-shadow: 0.3em 0.3em 1em rgba(0, 0, 0, 0.3);border-radius: 10px;'><td>"+r.rentId+"</td><td>"+r.cusId+"</td><td>"+r.pickUpDate+"</td><td>"+r.returnDate+"</td><td>"+r.pickUpVenue+"</td><td>"+r.returnVenue+"</td><td>"+r.statusOfReq+"</td><td>"+resp.data.statusOfReq+"</td></tr>");
+                $("#tblCustomerReq").append("<tr style='height: 71px;column-gap: 10px;gap: 10px;box-shadow: 0.3em 0.3em 1em rgba(0, 0, 0, 0.3);border-radius: 10px;'><td>"+r.rentId+"</td><td>"+r.cusId+"</td><td>"+r.pickUpDate+"</td><td>"+r.returnDate+"</td><td>"+r.pickUpVenue+"</td><td>"+r.returnVenue+"</td><td>"+r.statusOfReq+"</td><td>"+r.slipImgPath+"</td></tr>");
             }
 
         }
