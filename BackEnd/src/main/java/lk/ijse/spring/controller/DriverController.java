a8fa6e4ceab902912787ad87eb9f02bb7181b2ab
KavinduNimesh1030 <kavindunimesh286@gmail.com>
2023-02-27 11:07:59 +0530

    content updated

--- BackEnd/src/main/java/lk/ijse/spring/controller/DriverController.java
+++ BackEnd/src/main/java/lk/ijse/spring/controller/DriverController.java
@@ -1,17 +1,27 @@
 package lk.ijse.spring.controller;
 
+import lk.ijse.spring.dto.DriverDTO;
+import lk.ijse.spring.service.DriverService;
 import lk.ijse.spring.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+
 @RestController
 @RequestMapping("/Driver")
 @CrossOrigin
 public class DriverController {
+
+    @Autowired
+    DriverService driverService;
+
    @GetMapping
     public ResponseUtil getAllDrivers(){
-       return ResponseUtil("200","done",)
+       ArrayList<DriverDTO>all = driverService.getAllDriver();
+       return new ResponseUtil("200","done",all);
    }
 }
--- BackEnd/src/main/java/lk/ijse/spring/service/impl/DriverServiceImpl.java
+++ BackEnd/src/main/java/lk/ijse/spring/service/impl/DriverServiceImpl.java
@@ -1,7 +1,12 @@
 package lk.ijse.spring.service.impl;
 
 import lk.ijse.spring.dto.DriverDTO;
+import lk.ijse.spring.dto.RentalDTO;
+import lk.ijse.spring.repo.DriverRepo;
 import lk.ijse.spring.service.DriverService;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,8 +16,14 @@ import java.util.ArrayList;
 @Transactional
 public class DriverServiceImpl implements DriverService {
 
+    @Autowired
+    DriverRepo repo;
+
+    @Autowired
+    ModelMapper modelMapper;
+
     @Override
     public ArrayList<DriverDTO> getAllDriver() {
-        return null;
+       return  modelMapper.map(repo.findAll(),new TypeToken<ArrayList<DriverDTO>>(){}.getType());
     }
 }
